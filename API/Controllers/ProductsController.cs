fcb2371304957137d09003e170adfa5daa102365
chran <achristoffer@ymail.com>
2023-09-09 13:23:10 +0200

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _storeContext.Products.ToListAsync();
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _storeContext.Products.FindAsync(id);
+
+        }
+
+
+    }
+
+}
